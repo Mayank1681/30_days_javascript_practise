3be84f80df5b3981c6da8eae2bb4e6faae3f2110
Mayank1681 <mayankchoudhary385@gmail.com>
2023-01-10 12:27:15 +0530

    1 task remaining of 01_day

---
+++ 01_day/index.js
@@ -0,0 +1,40 @@
+/*
+function alertFunction() {
+    alert("30_days_of_javascript")
+}
+*/
+
+// comments can make code readable
+
+//  Welcome to 30DaysOfJavaScript
+
+/* 
+comments can make code readable, easy to reuse and informative
+*/
+
+// variables
+var a = "Mayank"; // assign string
+
+var b = 100;      // assign integer number
+var c = 1.12;     // assign floating-point number
+
+var d = true;     // assign boolean values to variable
+
+var e;            // not assinged a valued yet. So, it is undefined
+
+var f = null;     // assign null_value/empty_value
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);  // object type
+console.log(typeof f);
+
+
+var firstName = "mayank", lastName = "Choudhary", maritalStatus = "single", country = "India", myAge = 26;
+console.log(firstName, lastName, maritalStatus, country, myAge);
+
+console.log(`I am ${myAge} years old`);    // Template literals:- `String text ${expression}`
+ 
+// var text = document.getElementById("age");
